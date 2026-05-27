deba38e2f82a0b27602de91cfcb1ffce50f93cd7
Ashutosh Singh Patel <ashutosh.dev.hack@gmail.com>
2026-05-27 08:48:25 +0530

    Add: Keymap for Esc to switch from insert to normal mode

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Exit insert mode
-vim.keymap.set('i', '<C-[>', '<Esc>', { desc = 'Exit insert mode' })
+vim.keymap.set('i', '<Ctrl>[', '<Esc>', { desc = 'Exit insert mode' })
 
 -- Declare the path where lazy will clone plugin code
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
