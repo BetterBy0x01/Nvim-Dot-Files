6cdb0d9c3f5a45a514c2f53cea1ed614ed4b97bd
Ashutosh Singh Patel <ashutosh.dev.hack@gmail.com>
2026-05-30 20:34:01 +0530

    Add: Toggleterm plugin for terminal

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,25 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+        local status_ok, toggleterm = pcall(require, "toggleterm")
+        if not status_ok then
+         return
+        end
+
+        toggleterm.setup({
+         size = 20,
+         open_mapping = [[<c-\>]],
+         hide_numbers = true,
+         start_in_insert = true,
+         insert_mappings = true,
+         persist_size = true,
+         direction = "float",
+         close_on_exit = true,
+         shell = vim.o.shell,
+         float_opts = {
+          border = "curved",
+         },
+        })
+	end,
+}
